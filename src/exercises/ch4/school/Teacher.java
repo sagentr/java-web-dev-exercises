1f100d9d9fb6a4a04cb1aca1314b0cc89b3127d7
Sarah Gentry <sagentr5@gmail.com>
2021-09-22 13:20:57 -0500

    finished - modified getters and setters access points

---
+++ src/exercises/ch4/school/Teacher.java
@@ -0,0 +1,45 @@
+package exercises.ch4.school;
+
+public class Teacher {
+
+
+
+    private String firstName;
+    private String lastName;
+    private String subject;
+    private int yearsTeaching;
+
+
+    //getters and setters - what should be public should be able to change, what should be private is constant
+    public String getFirstName() {
+        return firstName;
+    }
+
+    private void setFirstName(String firstName) {
+        this.firstName = firstName;
+    }
+
+    public String getLastName() {
+        return lastName;
+    }
+
+    public void setLastName(String LastName) {
+        this.lastName = lastName;
+    }
+
+    public String getSubject() {
+        return subject;
+    }
+
+    public void setSubject(String subject) {
+        this.subject = subject;
+    }
+
+    public int getYearsTeaching() {
+        return yearsTeaching;
+    }
+
+    public void setYearsTeaching(int yearsTeaching) {
+        this.yearsTeaching = yearsTeaching;
+    }
+}
